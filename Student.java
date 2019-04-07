82202d5cdde8e33a4f14003ae428913b906eca62
JinhuaM <326217150@qq.com>
2019-04-07 10:29:33 +0800

    nice

---
+++ Student.java
@@ -0,0 +1,21 @@
+class Student{
+ String name;
+ int age;
+ int chinese;
+ int math;
+ int english;
+ int total(){
+	return chinese+math+english;
+}
+ int average(){
+	return (int)total()/3;
+}
+
+void setStudent(String n,int a,int c,int m,int e){
+	name=n;
+	age=a;
+	chinese=c;
+	math=m;
+	english=e;
+}
+}
